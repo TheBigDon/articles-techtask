ee7c1837a4122f3a865d87ed4706ac3ef33c79b2
Stepan <takistepan@gmail.com>
2023-04-27 17:27:14 +0300

    Добавлены модели статей и комментариев

---
+++ backend/models/article.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, Sequelize) => {
+  const Article = sequelize.define("article", {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    date_creation: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    date_modification: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  });
+  Article.associate = function (models) {
+    Article.hasMany(models.comment, {
+      onDelete: "CASCADE",
+    });
+  };
+
+  return Article;
+};
