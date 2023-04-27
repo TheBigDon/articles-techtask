3bb902aa066102c6c56e5fc5b8072ccf6c80ac3c
Stepan <takistepan@gmail.com>
2023-04-27 16:03:06 +0300

    Настроен sequelize

---
+++ backend/config/config.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
+
+module.exports = {
+  development: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: "blog_dev",
+    host: DB_HOST,
+    dialect: "postgres",
+  },
+  test: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: "blog_test",
+    host: DB_HOST,
+    dialect: "postgres",
+  },
+  production: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: "blog_prod",
+    host: DB_HOST,
+    dialect: "postgres",
+  },
+};
