a8e66813ccde8e44ecefe9ffe007fb2e5ffc9989
Stepan <takistepan@gmail.com>
2023-05-01 20:30:43 +0300

    Настроен cors

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const articleRouter = require("./routes/article.routes");
 const commentRouter = require("./routes/comment.routes");
 const analyticRouter = require("./routes/analytic.routes");
@@ -7,6 +8,7 @@ const PORT = 8080;
 
 const app = express();
 
+app.use(cors({ origin: "*", credentials: true, optionSuccessStatus: 200 }));
 app.use(express.json());
 app.use("", analyticRouter);
 app.use("", articleRouter);
